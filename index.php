223b966e9bb2e69f90d68be29870cd96d86f10c7
Mariana Terzieva <marianka.terzieva@gmail.com>
2026-06-14 17:12:42 +0300

    database test in index.php

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
+<!-- <?php
 header("Location: login.html");
 exit;
-?>
+?> -->
+
+<?php
+
+require 'database/connect_db.php';
+
+$db = (new DatabaseConnection())->getConnection();
+
+$stmt = $db->query("SELECT * FROM users");
+
+$rows = $stmt->fetchAll();
+
+echo "<table border='1'>";
+
+foreach ($rows as $row) {
+    echo "<tr>";
+
+    foreach ($row as $value) {
+        echo "<td>" . htmlspecialchars($value) . "</td>";
+    }
+
+    echo "</tr>";
+}
+
+echo "</table>";
