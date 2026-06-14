09e24b4a84d42e58769537b161e9c7451bb3717e
Mariana Terzieva <marianka.terzieva@gmail.com>
2026-06-14 21:43:08 +0300

    Database initialization for docker

--- index.php
+++ index.php
@@ -6,11 +6,11 @@ $db = (new DatabaseConnection())->getConnection();
 
 initializeDatabase($db);
 
-$stmt = $db->prepare("SELECT * FROM users WHERE role = 'teacher' LIMIT 1");
-$stmt->execute();
-$teacher = $stmt->fetch();
-echo "Проверка за съществуващ учител: " . ($teacher ? "Намерен" . $teacher['email'] : "Не е намерен") . "<br>";
+// $stmt = $db->prepare("SELECT * FROM users WHERE role = 'teacher' LIMIT 1");
+// $stmt->execute();
+// $teacher = $stmt->fetch();
+// echo "Проверка за съществуващ учител: " . ($teacher ? "Намерен " . $teacher['email'] : "Не е намерен") . "<br>";
 
-// header("Location: login.html");
+header("Location: login.html");
 exit;
 ?>
