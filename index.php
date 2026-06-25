f55ca164ef021f2af9231d1f9531083d76cfc86d
Mariana Terzieva <marianka.terzieva@gmail.com>
2026-06-25 22:26:54 +0300

    Update docker database with course edition.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once 'init_app.php';
 $db = (new DatabaseConnection())->getConnection();
 
 initializeDatabase($db);
+migrateDatabase($db);
 
 // $stmt = $db->prepare("SELECT * FROM users WHERE role = 'teacher' LIMIT 1");
 // $stmt->execute();
