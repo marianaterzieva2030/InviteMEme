29b7f64abeecdc7b7850dbf7c41cd41539d6f763
Mariana Terzieva <marianka.terzieva@gmail.com>
2026-06-25 22:32:37 +0300

    Test in index.php

--- index.php
+++ index.php
@@ -5,6 +5,19 @@ require_once 'init_app.php';
 $db = (new DatabaseConnection())->getConnection();
 
 initializeDatabase($db);
+
+$stmt = $db->prepare("SELECT * FROM users WHERE edition_id = '1' LIMIT 1");
+$stmt->execute();
+$users_edition = $stmt->fetch();
+if (!$users_edition) {
+    echo "Няма потребители с edition_id = 1";
+    exit;
+} else {
+    foreach ($users_edition as $u) {
+        echo "Потребител: " . $u['email'] . "<br>";
+    }
+}
+
 migrateDatabase($db);
 
 // $stmt = $db->prepare("SELECT * FROM users WHERE role = 'teacher' LIMIT 1");
